bb76e1224b21c3e6c88c76b1c3223501bb0955fe
Mukthy <muktheeswaran.m@gmail.com>
2022-05-29 22:10:44 +0530

    JS Notes

---
+++ js.js
@@ -0,0 +1,18 @@
+// Variable in JS:
+
+var george = "My name is George" + "!";
+
+george
+
+// Prompting in a browser to take input.
+prompt("What is your name")
+"Mukthy"
+
+// To give a popup alert
+alert("Mukthy")
+
+// Simple Calculator
+var first = prompt("enter first number");
+var second = prompt("enter first number");
+var sum = Number(first) + Number(second);
+alert("The sum is: " + sum)
